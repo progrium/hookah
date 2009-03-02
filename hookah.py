bb7dbb18efca1dfda793ab40f65e0f62c3061061
Jeff Lindsay <jeff@laprium.local>
2009-03-02 07:41:38 -0800

    first commit

---
+++ hookah.py
@@ -0,0 +1,34 @@
+import cherrypy
+import threading
+import urllib, urllib2
+import os
+
+PORT = 8000
+RETRIES = 3
+DELAY_MULTIPLIER = 5
+
+def post_and_retry(url, params, retry=0):
+    print "Posting [%s] to %s with %s" % (retry, url, params)
+    try:
+        urllib2.urlopen(url, urllib.urlencode(params) if len(params) else None)
+    except urllib2.HTTPError, e:
+        print e
+        if retry < RETRIES:
+            retry += 1
+            threading.Timer(retry * DELAY_MULTIPLIER, post_and_retry, args=[url, params, retry]).start()
+    
+class Root:
+    @cherrypy.expose
+    def default(self, *path, **params):
+        if path:
+            url = 'http://%s' % '/'.join(path)
+            threading.Thread(target=post_and_retry, args=[url, cherrypy.request.params]).start()
+            return "OK"
+        else:
+            return "No path."
+            
+if __name__ == '__main__':
+    cherrypy.config.update({'server.socket_port': PORT,})
+    root = Root()
+    cherrypy.quickstart(root)
+    
