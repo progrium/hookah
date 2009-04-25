5b4ee0a634ab3c520340dd8749ae7ac9170c0543
Jeff Lindsay <jeff@gliderbook.(none)>
2009-04-25 13:55:20 -0700

    dropped python threading in place of all twisted event stuff

--- hookah.py
+++ hookah.py
@@ -1,9 +1,9 @@
 from twisted.internet import reactor
 from twisted.web.resource import Resource
 from twisted.web.server import Site
-import threading
-import urllib, urllib2
-import os, sys
+from twisted.web import client
+import urllib
+import sys
 
 PORT = 8000 if len(sys.argv) < 2 else int(sys.argv[1])
 RETRIES = 3
@@ -11,14 +11,20 @@ DELAY_MULTIPLIER = 5
 
 def post_and_retry(url, params, retry=0):
     print "Posting [%s] to %s with %s" % (retry, url, params)
-    try:
-        urllib2.urlopen(url, urllib.urlencode(params) if len(params) else None)
-    except urllib2.HTTPError, e:
-        print e
-        if retry < RETRIES:
-            retry += 1
-            threading.Timer(retry * DELAY_MULTIPLIER, post_and_retry, args=[url, params, retry]).start()
+    postdata = urllib.urlencode(params)
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': str(len(postdata)),
+    }
+    client.getPage(url, method='POST' if len(postdata) else 'GET', headers=headers, postdata=postdata if len(postdata) else None).addCallbacks( \
+                    if_success, lambda reason: if_fail(reason, url, params, retry))
 
+def if_success(page): pass
+def if_fail(reason, url, params, retry):
+    print reason.getErrorMessage()
+    if retry < RETRIES:
+        retry += 1
+        reactor.callLater(retry * DELAY_MULTIPLIER, post_and_retry, url, params, retry)
 
 class HookahResource(Resource):
     def getChild(self, name, request):
@@ -29,7 +35,7 @@ class HookahResource(Resource):
             return
         if len(request.prepath):
             url = 'http://%s' % '/'.join(request.prepath)
-            threading.Thread(target=post_and_retry, args=[url, request.args]).start()
+            post_and_retry(url, request.args)
             return "OK"
         else:
             return "No destination."
